fdde343884fdefe92bfe32ba38625ab9e816460c
gang <gang@fangyingmobile.com>
2022-02-23 17:56:34 +0800

    router

--- src/layouts/SecurityLayout.tsx
+++ src/layouts/SecurityLayout.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import styles from './SecurityLayout.module.less';
 
 function SecurityLayout() {
     return (
         <div className={styles.securityLayoutWrap}>
-            安全校验
+            <Router>
+                <div>
+                    <Link to="/">首页</Link>
+                    <br />
+                    <Link to="/room">其他页</Link>
+                </div>
+                <Routes>
+                    <Route path="/room" element={<div>其他页</div>} />
+                    <Route path="/" element={<div>我是首页</div>} />
+                </Routes>
+            </Router>
         </div>
     )
 }
-
 export default SecurityLayout
